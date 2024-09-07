6cc09d776ba7ff87ac8f9fdf70d500f54068fe5c
jhons62 <joaojunio555@gmail.com>
2024-09-06 21:33:02 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,25 @@
+let dados= [
+    {       titulo:"Endrik",
+            descricao: "Endrik e uma das maiores promessas, foi descoberto no palmeiras e agora joga pelo real madri",
+            link:"https://pt.wikipedia.org/wiki/Endrick",
+            tags: "endrik goldenboy finalizador iluminado promissor"
+    },
+    
+    {
+            titulo:"Estevam",
+            descricao:"Estevam e uma das maiores promessas, foi descoberto no palmeiras e foi contratado pelo chealsea",
+            link:"https://pt.wikipedia.org/wiki/Estevam_Soares",
+            tags: "raio esquerdinha promissor predestinado"
+    },
+    
+    {
+    
+        titulo:"Roberto Carlos",
+        descricao:"foi um exelente jogador em sua possicao, sendo ate considerado um dos melhores do mundo em 1998 a 2002",
+        link:"https://pt.wikipedia.org/wiki/Roberto_Carlos_(futebolista)",
+        tags:"esquerdinha_de_ouro melhor_lateral_do_mundo top2002"
+    
+    }
+    
+    
+    ];
